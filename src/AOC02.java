c200b547b439bc5e415e08c2b1b46e1ea35877dc
I A <ia767@nyu.edu>
2021-12-29 13:07:48 -0500

    [2021-aoc2] adding java solution to day 2

---
+++ src/AOC02.java
@@ -0,0 +1,92 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class AOC02 {
+
+    public static void main(String[] args) {
+        // read input file
+        List<String> input = readFile("inputs/aoc2.txt");
+
+        // parse input, here creating 2 arrays
+        int inputLength = input.size();
+
+        int[] horizontalChange = new int[inputLength];
+        int[] verticalChange = new int[inputLength];
+
+        for (int i = 0; i < inputLength; i++){
+            int[] moveArray = parseInstruction(input.get(i));
+            horizontalChange[i] = moveArray[0];
+            verticalChange[i] = moveArray[1];
+        }
+
+        // compute & print solutions
+        int part1_score = part1(horizontalChange, verticalChange);  // 1746616
+        int part2_score = part2(inputLength, horizontalChange, verticalChange); // 1741971043
+
+        System.out.println("PART 1: " + part1_score);
+        System.out.println("PART 2: " + part2_score);
+    }
+
+    public static List<String> readFile(String filepath) {
+        InputStream is = AOC02.class.getClassLoader().getResourceAsStream(filepath);
+        List<String> input = new ArrayList<>();
+        try (Scanner scanner = new Scanner(is)) {
+            while (scanner.hasNextLine()) {
+                input.add(scanner.nextLine());
+            }
+        }
+        return input;
+    }
+
+    public static int[] parseInstruction(String fullInstruction)  {
+        String[] arrOfStr = fullInstruction.split(" ", 2);
+        String instruction = arrOfStr[0];
+        int value = Integer.parseInt(arrOfStr[1]);
+
+        switch (instruction) {
+            case "forward" -> {
+                return new int[]{value, 0};
+            }
+            case "down" -> {
+                return new int[]{0, value};
+            }
+            case "up" -> {
+                return new int[]{0, -value};
+            }
+            default -> {
+                return new int[]{0, 0};
+            }
+        }
+    }
+
+    public static int part1(int[] horizontalChange, int[] verticalChange)  {
+        int totalHorizontalChange = IntStream.of(horizontalChange).sum();
+        int totalDepthChange  = IntStream.of(verticalChange).sum();
+        return totalHorizontalChange * totalDepthChange;
+    }
+
+    public static int part2(int inputLength, int[] horizontalChange, int[] verticalChange)  {
+        int horizontalPos = 0;
+        int depth = 0;
+        int aim = 0;
+
+        for (int i = 0; i < inputLength; i++){
+            int forwardValue = horizontalChange[i];
+            int aimChange = verticalChange[i];
+
+            if (aimChange != 0){
+                aim += aimChange;
+            }
+            else{
+                horizontalPos += forwardValue;
+                depth += aim * forwardValue;
+            }
+        }
+
+        return horizontalPos * depth;
+    }
+
+}
